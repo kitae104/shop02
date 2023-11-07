6a9c619d80cf099159813d4a2ec41452b5ed7dd0
Kitae's Github <aqua0405@gmail.com>
2023-11-08 02:05:11 +0900

    Update ItemRepository.java

--- src/main/java/kr/inhatc/shop/repository/ItemRepository.java
+++ src/main/java/kr/inhatc/shop/repository/ItemRepository.java
@@ -3,11 +3,13 @@ package kr.inhatc.shop.repository;
 import kr.inhatc.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>,
+        QuerydslPredicateExecutor<Item> {
 
     List<Item> findByItemNm(String itemNm);     // 해당 이름에 대한 상품 리스트 가져오기
 
