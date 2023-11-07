1efdb8d44827879f83c7e89114f73683c56e139d
Kitae's Github <aqua0405@gmail.com>
2023-11-08 02:05:15 +0900

    Update ItemRepositoryTest.java

--- src/test/java/kr/inhatc/shop/repository/ItemRepositoryTest.java
+++ src/test/java/kr/inhatc/shop/repository/ItemRepositoryTest.java
@@ -1,5 +1,6 @@
 package kr.inhatc.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import kr.inhatc.shop.constant.ItemSellStatus;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.thymeleaf.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -122,5 +126,67 @@ class ItemRepositoryTest {
 //            System.out.println(item);
 //        }
 
+    }
+
+    // 데이터 준비
+    public void createItemList2(){
+        for(int i=1;i<=5;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+
+        for(int i=6;i<=10;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("querydsl 테스트2")
+    public void querydslTest2(){
+        createItemList2();
+
+        String itemDetail = "테스트";
+        Integer price = 10003;
+        String itemSellStatus = "SELL";
+
+        QItem item = QItem.item;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        builder.and(item.itemDetail.like("%" + itemDetail + "%"));
+        builder.and(item.price.gt(price));
+
+        if(StringUtils.equals(itemSellStatus, ItemSellStatus.SELL)){
+            builder.and(item.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = Pageable.ofSize(5).withPage(0);     // RageRequest.of(0, 5);
+
+        itemRepository.findAll(builder, pageable).forEach(i -> System.out.println(i));        
+        itemRepository.findAll(builder, pageable).forEach(System.out::println);
+
+        Page<Item> all = itemRepository.findAll(builder, pageable);
+        System.out.println("total elements : " + all.getTotalElements());
+        System.out.println("total pages : " + all.getTotalPages());
+
+        List<Item> content = all.getContent();
+        content.forEach(System.out::println);
+        for (Item item2 : content) {
+            System.out.println(item2);
+        }
     }
 }
